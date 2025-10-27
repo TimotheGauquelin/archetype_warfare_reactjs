089f2377071aae3e0b233941307fc083f60d9b68
TimotheSymbolIt <timothe.gauquelin@symbol-it.fr>
2025-10-27 21:41:30 +0100

    wip

--- src/api/api_aw.jsx
+++ src/api/api_aw.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api_aw = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
-    ? process.env.REACT_APP_BACKEND_URL + "/api"
+    ? process.env.REACT_APP_BACKEND_URL
     : "http://localhost:8889/api" // Pour le développement local
 });
 
