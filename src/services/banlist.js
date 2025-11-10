25c0f5d9bfc7dd3e001f25647ad32caa7a3e9943
TimotheSymbolIt <timothe.gauquelin@symbol-it.fr>
2025-11-10 15:27:05 +0100

    wip

--- src/services/banlist.js
+++ src/services/banlist.js
@@ -1,6 +1,6 @@
 import api_aw from "../api/api_aw";
 import { api_aw_token } from "../api/api_aw_token";
-import { URL_BACK_ADD_BANLIST, URL_BACK_DELETE_BANLIST, URL_BACK_GET_BANLIST, URL_BACK_GET_BANLISTS, URL_BACK_GET_CURRENT_BANLIST, URL_BACK_UPDATE_BANLIST } from "../constant/urlsBack";
+import { URL_BACK_ADD_BANLIST, URL_BACK_DELETE_BANLIST, URL_BACK_GET_BANLISTS, URL_BACK_GET_CURRENT_BANLIST, URL_BACK_UPDATE_BANLIST } from "../constant/urlsBack";
 import { URL_FRONT_ADMIN_BANLISTS } from "../constant/urlsFront";
 
 export const getCurrentBanlist = (setBanlist) => {
@@ -29,12 +29,10 @@ export const getBanlists = (setBanlists) => {
 };
 
 export const getBanlistById = async(id, setBanlist) => {
-    console.log(id);
     try {
         api_aw.get(`/banlists/${id}`).then((response) => {
-            console.log("ici", response);
             if (response.status === 200) {
-                console.log(response.data.data);
+                console.log("ici", response.data.data);
                 setBanlist(response.data.data);
             }
         });
